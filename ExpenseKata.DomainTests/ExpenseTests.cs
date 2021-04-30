e201713ce940da122e540c6f012163e2979336cd
Joffrey Truyen <jtruyen@cirilgroup.com>
2021-04-30 22:04:03 +0200

    Refacto tests

--- ExpenseKata.DomainTests/ExpenseTests.cs
+++ ExpenseKata.DomainTests/ExpenseTests.cs
@@ -11,30 +11,35 @@ namespace ExpenseKata.DomainTests
         [Fact]
         public void CreateExpense_EmptyCommentaryShoudThrowException()
         {
-            string comment = string.Empty;
-            var exception = Assert.Throws<BusinessRuleValidationException>(() => Expense.Create(null, comment, new DateTime()));
+            var builder = new EpenseBuilder();
+            builder.WithComment(string.Empty);
+            var exception = Assert.Throws<BusinessRuleValidationException>(() => builder.Build());
             Assert.Equal(ExpenseValidationConstants.ExpenseShouldHaveCommentMessage, exception.Message);
         }
 
         [Fact]
         public void CreateExpense_FutureExpenseShoudThrowException()
         {
-            string comment = "Default";
             DateTime expenseDate = new DateTime(2021, 6, 1);
             var provider = new DateTimeProviderStub(new DateTime(2021, 5, 1));
-            var exception = Assert.Throws<BusinessRuleValidationException>(() => Expense.Create(provider, comment, expenseDate));
+
+            var builder = new EpenseBuilder();
+            builder.WithExpenseDate(expenseDate);
+            builder.WithDateTimeProvider(provider);
+            var exception = Assert.Throws<BusinessRuleValidationException>(() => builder.Build());
             Assert.Equal(ExpenseValidationConstants.ExpenseCannotBeInFuture, exception.Message);
         }
 
         [Fact]
         public void CreateExpense_PastExpense_ShoudNotThrowException()
         {
-            string comment = "Default";
             DateTime expenseDate = new DateTime(2021, 4, 25);
             var provider = new DateTimeProviderStub(new DateTime(2021, 5, 1));
 
-
-            var exception = Record.Exception(() => Expense.Create(provider, comment, expenseDate));
+            var builder = new EpenseBuilder();
+            builder.WithExpenseDate(expenseDate);
+            builder.WithDateTimeProvider(provider);
+            var exception = Record.Exception(() => builder.Build());
             Assert.Null(exception);
         }
 
@@ -50,5 +55,44 @@ namespace ExpenseKata.DomainTests
         }
 
         public DateTime Now => _testDate;
+    }
+
+    internal class EpenseBuilder
+    {
+        private string _comment;
+        private IDateTimeProvider _dateTimeProvider;
+        private DateTime _expenseDate;
+
+        public EpenseBuilder()
+        {
+            SetDefaultValue();
+        }
+
+        private void SetDefaultValue()
+        {
+            _comment = "default";
+            _dateTimeProvider = new DateTimeProviderStub(new DateTime(2021, 5, 1));
+            _expenseDate = new DateTime(2021, 4, 1);
+        }
+
+        public Expense Build()
+        {
+            return Expense.Create(_dateTimeProvider, _comment, _expenseDate);
+        }
+
+        public void WithComment(string comment)
+        {
+            _comment = comment;
+        }
+
+        public void WithExpenseDate(DateTime date)
+        {
+            _expenseDate = date;
+        }
+
+        public void WithDateTimeProvider(IDateTimeProvider provider)
+        {
+            _dateTimeProvider = provider;
+        }
     }
 }
